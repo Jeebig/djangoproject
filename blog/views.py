eb60b8e533d0e9b3fb6b061946a328904cd23949
ya_Rei <petro_kt00@student.itstep.org>
2025-09-17 20:07:39 +0300

    Initialize Django project structure with basic settings, models, views, and templates

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
 def index(request):
-    return render(request, 'blog/index.html', {'title': 'Blog Home'})
+    posts = Post.objects.all().order_by('-created_at')
+    return render(request, 'blog/index.html', {
+        'title': 'Blog Home',
+        'posts': posts,
+        'user': request.user,
+    })
